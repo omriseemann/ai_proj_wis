8c36533ac746b3c8dd5d2dd2c5baaa3fe875ad38
Omri Seemann <omri.seemann@gmail.com>
2019-06-18 15:42:27 +0300

    added capcha generator

--- function_gen.py
+++ function_gen.py
@@ -10,6 +10,11 @@ import torch
 import torch.nn as nn
 import numpy as np
 import matplotlib.pyplot as plt
+from captcha.image import ImageCaptcha
+# pip install captcha
+import random
+import string
+
 
 class FunctionalGen:
     def generateImage(self):
@@ -20,6 +25,7 @@ class FunctionalGen:
     
     def generateBatch(self,n):
         raise Exception('Need to implement this function.')
+
 
 class ScintImageGen(FunctionalGen):
     
@@ -34,13 +40,34 @@ class ScintImageGen(FunctionalGen):
     def generateImage(self):
         xm,ym = np.meshgrid(range(self.nx), range(self.ny))
         I = np.round(np.random.rand(self.nx,self.ny)*self.noise_level)
-        
         return I
 
     def generateBatch(self, n):
         pass
 
 
+class CaptchaGen_OS(FunctionalGen):
+    
+    def __init__(self, string_length=6):
+        self.string_length = string_length
+        self.generator = ImageCaptcha()
+        return
+    
+    def generateRandomString(self):
+        letters = string.ascii_lowercase
+        return ''.join(random.choice(letters) for i in range(self.string_length))
+    
+    def generateImage(self):
+        s = self.generateRandomString()
+        I = self.generator.generate_image(s)
+        return I, s
+
+    def generateBatch(self, n):
+        pass
+
+
 if __name__ == "__main__":
-    main = ScintImageGen()
-    plt.imshow(main.generateImage())
+    main = CaptchaGen_OS()
+    data,s = main.generateImage()
+    plt.imshow(data)
+    print(s)
