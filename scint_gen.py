a3eb3c56d43884e770aa70b5c2d573296a024c84
omri seemann <45620645+omriseemann@users.noreply.github.com>
2019-07-14 11:50:21 +0300

    Omri model change (#6)
    * model change for scint
    
    * fixed arch and tested for captcha
    
    * testing scynt

--- scint_gen.py
+++ scint_gen.py
@@ -18,7 +18,7 @@ class ScintImageGen(FunctionalGenerator):
     def __init__(self, pixel_Nx=2048, pixel_Ny=2048, rbright=50,
                  noise_level=(0.1, 0.5), corr_size=2,
                  spot_size_range=(10, 20), distance_factor=(1.5, 3),
-                 spot_Nx=80, spot_Ny=80, tan_angle=0.1):
+                 spot_Nx=40, spot_Ny=40, tan_angle=0.1):
         self.pixel_Nx = pixel_Nx
         self.pixel_Ny = pixel_Ny
         self.corr_size = corr_size
@@ -27,8 +27,8 @@ class ScintImageGen(FunctionalGenerator):
         self.spot_Ny = spot_Ny
         self.distance_factor = distance_factor
         data_x, data_y = np.meshgrid(range(pixel_Nx), range(pixel_Ny))
-        self.data_x = data_x
-        self.data_y = data_y
+        self.data_x = torch.FloatTensor(data_x)
+        self.data_y = torch.FloatTensor(data_y)
         self.noise_level = noise_level
         noise_map = np.abs(np.random.randn(
                 2*pixel_Nx // corr_size,
@@ -41,11 +41,11 @@ class ScintImageGen(FunctionalGenerator):
         self.input_shape = image.shape
         self.output_shape = t.shape
 
-    def generateImage(self):
+    def generateImage(self, params=None):
         tan_angle1 = (2*np.random.rand()-1)*self.tan_angle
         tan_angle2 = (2*np.random.rand()-1)*self.tan_angle
-        center_x = (1/4+np.random.rand()/2) * self.pixel_Nx
-        center_y = (1/4+np.random.rand()/2) * self.pixel_Ny
+        center_x = (0.5 + 0.25 * (np.random.rand()-0.5)) * self.pixel_Nx
+        center_y = (0.5 + 0.25 * (np.random.rand()-0.5)) * self.pixel_Ny
         spot_size = self.spot_size_range[0] + np.random.rand() * (
                 self.spot_size_range[1] - self.spot_size_range[0])
         distance1 = (self.distance_factor[0] + np.random.rand() * (
@@ -83,12 +83,10 @@ class ScintImageGen(FunctionalGenerator):
                                         y >= 0):
                     data[x:x+spot_map.shape[0],
                          y:y+spot_map.shape[1]] = spot_map * np.random.rand()
-        target = [center_x/self.pixel_Nx, center_y/self.pixel_Ny,
-                  distance1/spot_size, distance2/spot_size, tan_angle1,
-                  tan_angle2, spot_size]
-        target = torch.tensor(target)
-        target = target.unsqueeze(1)
-        target = target.unsqueeze(1)
+        target = torch.tensor([center_x, center_y, distance1, distance2,
+                               tan_angle1*distance1, tan_angle2*distance2,
+                               spot_size])
+        target = target.unsqueeze(-1).unsqueeze(-1)
         data = torch.FloatTensor(data)
         data += noise_map.float()
         data = data.unsqueeze(0)
@@ -102,33 +100,31 @@ class ScintImageGen(FunctionalGenerator):
         return loss
 
     def error(self, output, target):
-        loss = (target-output) / target
+        loss = (target-output)/(target+output)
         return loss.abs().mean()
 
     def plot_examples(self, _input, target, output):
         k = 0
-        for im, o, t in zip(_input, output, target):
+        for im, t, o in zip(_input, target, output):
             k += 1
             plt.figure(k)
             plt.imshow(im[0])
-            o = o.squeeze(1).squeeze(1).detach().numpy()
-            center_x = o[0] * self.pixel_Nx
-            center_y = o[1] * self.pixel_Ny
-            distance1 = o[2] * o[6]
-            distance2 = o[3] * o[6]
-            tan_angle1 = o[4]
-            tan_angle2 = o[5]
+            cx = o[0]
+            cy = o[1]
+            d1 = o[2]
+            d2 = o[3]
+            a1 = o[4]/d1
+            a2 = o[5]/d2
             xv = []
             yv = []
             for i in range(self.spot_Nx):
                 for j in range(self.spot_Ny):
-                    x = center_x + ((i - self.spot_Nx // 2) +
-                                    (j - self.spot_Ny // 2) *
-                                    tan_angle1) * distance1
-                    y = center_y + ((i - self.spot_Nx // 2) * tan_angle2 +
-                                    (j - self.spot_Ny // 2)) * distance2
+                    x = cx + ((i - self.spot_Nx // 2) + (j - self.spot_Ny // 2)
+                              * a1) * d1
+                    y = cy + ((i - self.spot_Ny // 2) * a2 +
+                              (j - self.spot_Ny // 2)) * d2
                     if (x < self.pixel_Nx) and (y < self.pixel_Ny) and (
-                            x >= 0) and (y >= 0):
+                                x >= 0) and (y >= 0):
                         xv.append(x)
                         yv.append(y)
             plt.plot(yv, xv, '+y')
