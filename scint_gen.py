07e1ba21444b944309cb2cde0799ec9a432f7978
Omri Seemann <omri.seemann@gmail.com>
2019-07-14 11:49:22 +0300

    testing scynt

--- scint_gen.py
+++ scint_gen.py
@@ -18,7 +18,7 @@ class ScintImageGen(FunctionalGenerator):
     def __init__(self, pixel_Nx=2048, pixel_Ny=2048, rbright=50,
                  noise_level=(0.1, 0.5), corr_size=2,
                  spot_size_range=(10, 20), distance_factor=(1.5, 3),
-                 spot_Nx=80, spot_Ny=80, tan_angle=0.1):
+                 spot_Nx=40, spot_Ny=40, tan_angle=0.1):
         self.pixel_Nx = pixel_Nx
         self.pixel_Ny = pixel_Ny
         self.corr_size = corr_size
@@ -41,11 +41,11 @@ class ScintImageGen(FunctionalGenerator):
         self.input_shape = image.shape
         self.output_shape = t.shape
 
-    def generateImage(self):
+    def generateImage(self, params=None):
         tan_angle1 = (2*np.random.rand()-1)*self.tan_angle
         tan_angle2 = (2*np.random.rand()-1)*self.tan_angle
-        center_x = (1/4+np.random.rand()/2) * self.pixel_Nx
-        center_y = (1/4+np.random.rand()/2) * self.pixel_Ny
+        center_x = (0.5 + 0.25 * (np.random.rand()-0.5)) * self.pixel_Nx
+        center_y = (0.5 + 0.25 * (np.random.rand()-0.5)) * self.pixel_Ny
         spot_size = self.spot_size_range[0] + np.random.rand() * (
                 self.spot_size_range[1] - self.spot_size_range[0])
         distance1 = (self.distance_factor[0] + np.random.rand() * (
@@ -68,7 +68,6 @@ class ScintImageGen(FunctionalGenerator):
         noise_level = self.noise_level[0] + (
                 self.noise_level[1] - self.noise_level[0]) * np.random.rand()
         noise_map = noise_map * noise_level
-        target = torch.tensor(np.zeros((3, self.spot_Nx, self.spot_Ny)))
         for i in range(self.spot_Nx):
             for j in range(self.spot_Ny):
                 x = center_x + ((i - self.spot_Nx // 2) +
@@ -84,15 +83,12 @@ class ScintImageGen(FunctionalGenerator):
                                         y >= 0):
                     data[x:x+spot_map.shape[0],
                          y:y+spot_map.shape[1]] = spot_map * np.random.rand()
-                    target[0, i, j] = x + spot_map.shape[0]//2 - 1
-                    target[1, i, j] = y + spot_map.shape[1]//2 - 1
-                    target[2, i, j] = 1
+        target = torch.tensor([center_x, center_y, distance1, distance2,
+                               tan_angle1*distance1, tan_angle2*distance2,
+                               spot_size])
+        target = target.unsqueeze(-1).unsqueeze(-1)
         data = torch.FloatTensor(data)
         data += noise_map.float()
-        '''
-        data = torch.cat((data.unsqueeze(0), self.data_x.unsqueeze(0),
-                          self.data_y.unsqueeze(0)))
-        '''
         data = data.unsqueeze(0)
         return data.float(), target.float()
 
@@ -104,8 +100,7 @@ class ScintImageGen(FunctionalGenerator):
         return loss
 
     def error(self, output, target):
-        loss = (target-output)
-        loss = loss[0:1, :, :]
+        loss = (target-output)/(target+output)
         return loss.abs().mean()
 
     def plot_examples(self, _input, target, output):
@@ -114,13 +109,22 @@ class ScintImageGen(FunctionalGenerator):
             k += 1
             plt.figure(k)
             plt.imshow(im[0])
+            cx = o[0]
+            cy = o[1]
+            d1 = o[2]
+            d2 = o[3]
+            a1 = o[4]/d1
+            a2 = o[5]/d2
             xv = []
             yv = []
             for i in range(self.spot_Nx):
                 for j in range(self.spot_Ny):
-                    x = o[0, i, j]
-                    y = o[1, i, j]
-                    if o[2, i, j] > 0:
+                    x = cx + ((i - self.spot_Nx // 2) + (j - self.spot_Ny // 2)
+                              * a1) * d1
+                    y = cy + ((i - self.spot_Ny // 2) * a2 +
+                              (j - self.spot_Ny // 2)) * d2
+                    if (x < self.pixel_Nx) and (y < self.pixel_Ny) and (
+                                x >= 0) and (y >= 0):
                         xv.append(x)
                         yv.append(y)
             plt.plot(yv, xv, '+y')
--- test_synt.py
+++ test_synt.py
@@ -7,7 +7,7 @@ Created on Wed Jun 26 14:53:38 2019
 """
 
 from scint_gen import ScintImageGen
-from models import ModelCNN, ResModule
+from models import ResModule
 from learner import LearnerGenerative
 
 if __name__ == '__main__':
@@ -15,11 +15,11 @@ if __name__ == '__main__':
     I, t = gen.generateBatch(2)
     # gen.plot_examples(I, t, t)
     a = gen.lossBatch(t, t)
-    learner = LearnerGenerative(gen, ModelCNN, ResModule)
-    learner.reset(lr_start=1e-2)
+    learner = LearnerGenerative(gen, ResModule)
+    learner.reset(lr_start=1e-3)
     learner.save_params['name'] = 'scint_test'
     learner.load()
     o = learner.model(I)
-    learner.learn(500, 50)
+    learner.learn(1000, 20)
     learner.save()
     learner.plot()
